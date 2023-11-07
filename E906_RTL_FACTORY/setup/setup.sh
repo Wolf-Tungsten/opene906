da67b58ccc888a813bbd35e85488af2fa24567f5
wolf-tungsten <gaoruihao20s@ict.ac.cn>
2023-11-07 11:09:59 +0800

    setup.sh

---
+++ E906_RTL_FACTORY/setup/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/csh
+
+export pattern="\/setup"`echo '$'`
+export CODE_BASE_PATH=`pwd | perl -pe "s/$pattern//"`
+echo "Root of code base has been specified as:\n    $CODE_BASE_PATH"
+
+
+
+
+
